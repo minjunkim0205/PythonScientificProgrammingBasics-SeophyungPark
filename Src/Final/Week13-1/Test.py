16c464fc730044427c2813d645933d8b1950f946
minjunkim0205 <minjunkim0205@gmail.com>
2023-11-22 15:10:46 +0900

    Update Week13-1

--- Src/Final/Week13-1/Test.py
+++ Src/Final/Week13-1/Test.py
@@ -23,24 +23,28 @@ vertex_colors = np.array([[255,0,0],
 r_idx = np.arange(rows).reshape(-1,1)
 c_idx = np.arange(cols).reshape(1,-1)
 '''==========================================================================='''
-img1 = np.full((rows, cols, 3), 0, np.uint8)
-# Upper part of line AB
+img = np.full((rows, cols, 3), 0, np.uint8)
+# Upper,Lower part of line AB
 slopeAB = (A-B)[1] / (A-B)[0]
-AB = (c_idx > (slopeAB * (r_idx - A[0]) + A[1]))
-# Lower part of line BC
-slopeBC = (C-B)[1] / (C-B)[0]
-BC = c_idx < (slopeBC * (r_idx - B[0]) + B[1])
-# Right part of line CD
-CD = r_idx > C[0]
-# DE
-slopeDE = (E-D)[1] / (E-D)[0]
-DE = c_idx > (slopeDE * (r_idx - D[0]) + D[1])
-# EA
-slopeEA = (A-E)[1] / (A-E)[0]
-EA = c_idx < (slopeEA * (r_idx - E[0]) + E[1])
+idxUpperAB = (c_idx > (slopeAB * (r_idx - A[0]) + A[1]))
+idxLowerAB = (c_idx < (slopeAB * (r_idx - A[0]) + A[1]))
+# Upper,Lower part of line BC
+slopeBC = (B-C)[1] / (B-C)[0]
+idxUpperBC = c_idx > (slopeBC * (r_idx - B[0]) + B[1])
+idxLowerBC = c_idx < (slopeBC * (r_idx - B[0]) + B[1])
+# Upper,Lower part of line CD
+idxUpperCD = r_idx > C[0]
+idxLowerCD = r_idx < C[0]
+# Upper,Lower part of line DE
+slopeDE = (D-E)[1] / (D-E)[0]
+idxUpperDE = c_idx > (slopeDE * (r_idx - D[0]) + D[1])
+idxLowerDE = c_idx < (slopeDE * (r_idx - D[0]) + D[1])
+# Upper,Lower part of line EA
+slopeEA = (E-A)[1] / (E-A)[0]
+idxUpperEA = c_idx > (slopeEA * (r_idx - E[0]) + E[1])
+idxLowerEA = c_idx < (slopeEA * (r_idx - E[0]) + E[1])
 '''==========================================================================='''
-idxABCD = AB & BC & CD
-idxBCDE = BC & CD & DE
-img1[idxBCDE] = vertex_colors[0]
-plt.imshow(img1)
+idxABCDE = idxUpperAB & idxLowerBC & idxUpperCD & idxUpperDE & idxLowerEA
+img[idxABCDE] = vertex_colors[4]
+plt.imshow(img)
 plt.show()
